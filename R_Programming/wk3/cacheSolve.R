f6e5c5d6515bdd56e95c93c3dce356bd61349a6e
cootem <michael.coote@poweradvocate.com>
2016-01-21 11:25:28 -0500

    wk3 edits to cache and solve functions

---
+++ R_Programming/wk3/cacheSolve.R
@@ -0,0 +1,11 @@
+cacheSolve <- function(x, ...) {
+        m <- x$getSolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setSolve(m)
+        m
+}
---
+++ R_Programming/wk3/makeCacheMatrix.R
@@ -0,0 +1,13 @@
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setSolve <- function(solve) m <<- solve
+        getSolve <- function() m
+        list(set = set, get = get,
+             setSolve = setSolve,
+             getSolve = getSolve)
+}
